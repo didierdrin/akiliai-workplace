74c5ebf39c40db2efc195e51b637d57170a6913a
Didier Nsengiyumva <nsedidier@gmail.com>
2025-09-15 21:17:34 +0200

    latest updates 917

---
+++ akiliai-dashboard/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
+
+module.exports = nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   }
+// };
+
+// export default nextConfig;
